daab5be4d041a4b7ffbd356ed556056cf28d6ecb
Mari <marur00911@gmail.com>
2025-07-10 17:31:58 -0300

    Implementación de la estructura básica de la aplicación con componentes y estilos para el estudiante

--- src/app/student/student.ts
+++ src/app/student/student.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-student',
   imports: [],
-  templateUrl: './student.html',
-  styleUrl: './student.css'
+  template: `
+  <h1>Counter: {{ counter }}</h1>
+  <button (click)="increaseBy(1)">+1</button>
+  <button (click)="increaseBy(-1)">-1</button>
+  <button (click)="resetCounter()">Reset</button>
+  `,
+  // templateUrl: './student.html',
+  styleUrls: ['./student.css'],
 })
 export class Student {
+  counter = 10;
 
+  increaseBy(value: number) {
+    this.counter += value;
+  }
+
+  resetCounter() {
+    this.counter = 10;
+  }
 }
